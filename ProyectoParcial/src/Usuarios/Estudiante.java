c055cb744b0971acb1dbd5df6391de8d741fd657
Daniel Alvarado <danapela@espol.edu.ec>
2019-11-14 13:49:03 -0500

    getters and setters

--- ProyectoParcial/src/Usuarios/Estudiante.java
+++ ProyectoParcial/src/Usuarios/Estudiante.java
@@ -10,5 +10,29 @@ package Usuarios;
  * @author DanielAlvaradoPelaez
  */
 public class Estudiante {
+    private String nombre;
+    private int matricula;
+
+    public Estudiante(String nombre, int matricula) {
+        this.nombre = nombre;
+        this.matricula = matricula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+    
     
 }
